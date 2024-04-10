ccde7979997e10368c5c0faa2f72422c5a733811
hayama <hayamaroom@gmail.com>
2024-04-10 11:13:29 +0800

    add sign in

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={cn(
-        "min-h-screen bg-background font-sans antialiased pt-12",
+        "min-h-screen bg-background font-sans antialiased mt-12",
         font.variable
       )}>
         <ThemeProvider
