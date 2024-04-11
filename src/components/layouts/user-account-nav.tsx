093f4b549255cb15cf8a107b7acab15666253a9d
hayama <hayamaroom@gmail.com>
2024-04-11 11:13:00 +0800

    change icons

--- src/components/layouts/user-account-nav.tsx
+++ src/components/layouts/user-account-nav.tsx
@@ -1,8 +1,11 @@
+'use client'
+
 import { User } from 'next-auth'
 import React from 'react'
-import LogoutButton from './logout-btn';
-import { DropdownMenu, DropdownMenuTrigger } from '../ui/dropdown-menu';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from '../ui/dropdown-menu';
 import UserAvatar from './user-avatar';
+import Link from 'next/link';
+import { signOut } from 'next-auth/react';
 
 interface UserAccountNavProps extends React.HTMLAttributes<HTMLDivElement> {
   user: Pick<User, 'name' | 'image' | 'email'>
@@ -18,6 +21,46 @@ export default function UserAccountNav({ user }: UserAccountNavProps) {
           className='h-8 w-8'
         />
       </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <div className='gap-3 p-2'>
+          <div className='space-y-1 leading-none'>
+            {user.name && <p className='font-medium'>{user.name}</p>}
+            {user.email && (
+              <p className='w-11/12 truncate text-sm text-muted-foreground'>
+                {user.email} 
+              </p>
+            )}
+          </div>
+        </div>
+        
+        <DropdownMenuSeparator />
+        
+        <DropdownMenuItem asChild>
+          <Link href={'/'}>Feed</Link>
+        </DropdownMenuItem>
+
+        <DropdownMenuItem asChild>
+          <Link href={'/r/create'}>Create Community</Link>
+        </DropdownMenuItem>
+
+        <DropdownMenuItem asChild>
+          <Link href={'/setting'}>Setting</Link>
+        </DropdownMenuItem>
+
+        <DropdownMenuSeparator />
+
+        <DropdownMenuItem
+          className='cursor-pointer'
+          onSelect={(event) => {
+            event.preventDefault()
+            signOut({
+              callbackUrl: `${window.location.origin}/sign-in`,
+            })
+          }}>
+          Sign out
+          
+        </DropdownMenuItem>
+      </DropdownMenuContent>
     </DropdownMenu>
   )
 }
--- src/components/layouts/user-auth-form.tsx
+++ src/components/layouts/user-auth-form.tsx
@@ -10,15 +10,14 @@ import { signIn } from 'next-auth/react';
 export default function UserAuthForm() {
   const [ isLoading, setIsLoading ] = useState(false);
 
-  const loginWthGithub = async () => {
+  const loginWith = async (provider: string) => {
     setIsLoading(true)
     try {
-      await signIn('google', { callbackUrl: '/'}).then(res => {
-        toast.success('Successfully logged in with Google')
-      })
+      await signIn(provider, { callbackUrl: '/'})
+      toast.success(`Successfully logged in with ${provider.toUpperCase()}`)
     } catch (error) {
       toast.error('Something went wrong', {
-        description: 'There was an error logging in with Google',
+        description: `There was an error logging in with ${provider.toUpperCase()}`,
       })
     } finally {
       setIsLoading(false)
@@ -27,16 +26,26 @@ export default function UserAuthForm() {
 
   return (
     <div className={cn(
-      'flex justify-center '
+      'space-y-2 '
     )}>
       <Button 
         isLoading={isLoading}
         className='w-full'
-        onClick={loginWthGithub}
+        onClick={() => loginWith('google')}
         disabled={isLoading}
       >
         {isLoading ? null : <Icons.google className='h-4 w-4 mr-2'/>}
         Google
+      </Button>
+
+      <Button 
+        isLoading={isLoading}
+        className='w-full'
+        onClick={() => loginWith('github')}
+        disabled={isLoading}
+      >
+        {isLoading ? null : <Icons.github className='h-4 w-4 mr-2'/>}
+        Github
       </Button>
     </div>
   )
