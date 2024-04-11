94ac37f7f3f67daf8fad4b9625b01350acdbaf90
hayama <hayamaroom@gmail.com>
2024-04-11 18:51:42 +0800

    change auth modal

--- src/app/@authModal/(.)sign-in/page.tsx
+++ src/app/@authModal/(.)sign-in/page.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 export default function SignInModal() {
   return (
-    <div className='fixed inset-0 z-10 '>
+    <div className='fixed inset-0 z-10 bg-background/20 backdrop-blur'>
       <div className='container flex items-center h-full max-w-lg mx-auto '>
         <div className='relative bg-background border w-full h-fit py-20 px-2 rounded-lg'>
           <div className='absolute top-4 right-4'>
