33c227ce3c801a3774f924c0d8adce275c39b1c1
hayama <hayamaroom@gmail.com>
2024-04-11 22:30:56 +0800

    change home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import CustomFeed from "@/components/custom-feed";
+import GeneralFeed from "@/components/general-feed";
 import Header from "@/components/layouts/header";
 import {LoadingHeaderSkeleton} from "@/components/layouts/skeletons";
 import Posts from "@/components/posts";
@@ -5,73 +7,45 @@ import { PostsSkeleton } from "@/components/skeletons";
 import { buttonVariants } from "@/components/ui/button";
 import { auth } from "@/lib/auth-config";
 import { cn } from "@/lib/utils";
+import { HomeIcon } from "lucide-react";
 import Link from "next/link";
 import { Suspense } from "react";
 
 export default async function Home() {
   const session = await auth()
-  const user = session?.user;
   
   return (
-    <section>
-      test
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit culpa, aliquid nam ab distinctio provident earum voluptas delectus tempora voluptate? Velit repudiandae voluptate vel. Nisi voluptatibus ipsum a omnis ullam.
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci quo impedit est illo, nisi, eligendi, vero non asperiores numquam recusandae fugit esse! Vero, officia dolores minima placeat quod atque ullam.
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas assumenda cum iste nam, blanditiis laboriosam, omnis esse illo eaque molestias neque rem iure maxime maiores praesentium nemo accusamus, tenetur fuga?
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam nulla doloribus asperiores quis provident omnis rerum quia autem dicta, sed necessitatibus quas ab, voluptatum accusantium error, earum dolores. Illum, ut?
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit culpa, aliquid nam ab distinctio provident earum voluptas delectus tempora voluptate? Velit repudiandae voluptate vel. Nisi voluptatibus ipsum a omnis ullam.
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci quo impedit est illo, nisi, eligendi, vero non asperiores numquam recusandae fugit esse! Vero, officia dolores minima placeat quod atque ullam.
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas assumenda cum iste nam, blanditiis laboriosam, omnis esse illo eaque molestias neque rem iure maxime maiores praesentium nemo accusamus, tenetur fuga?
-      
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit culpa, aliquid nam ab distinctio provident earum voluptas delectus tempora voluptate? Velit repudiandae voluptate vel. Nisi voluptatibus ipsum a omnis ullam.
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci quo impedit est illo, nisi, eligendi, vero non asperiores numquam recusandae fugit esse! Vero, officia dolores minima placeat quod atque ullam.
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas assumenda cum iste nam, blanditiis laboriosam, omnis esse illo eaque molestias neque rem iure maxime maiores praesentium nemo accusamus, tenetur fuga?
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit culpa, aliquid nam ab distinctio provident earum voluptas delectus tempora voluptate? Velit repudiandae voluptate vel. Nisi voluptatibus ipsum a omnis ullam.
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci quo impedit est illo, nisi, eligendi, vero non asperiores numquam recusandae fugit esse! Vero, officia dolores minima placeat quod atque ullam.
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas assumenda cum iste nam, blanditiis laboriosam, omnis esse illo eaque molestias neque rem iure maxime maiores praesentium nemo accusamus, tenetur fuga?
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit culpa, aliquid nam ab distinctio provident earum voluptas delectus tempora voluptate? Velit repudiandae voluptate vel. Nisi voluptatibus ipsum a omnis ullam.
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci quo impedit est illo, nisi, eligendi, vero non asperiores numquam recusandae fugit esse! Vero, officia dolores minima placeat quod atque ullam.
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas assumenda cum iste nam, blanditiis laboriosam, omnis esse illo eaque molestias neque rem iure maxime maiores praesentium nemo accusamus, tenetur fuga?
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit culpa, aliquid nam ab distinctio provident earum voluptas delectus tempora voluptate? Velit repudiandae voluptate vel. Nisi voluptatibus ipsum a omnis ullam.
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci quo impedit est illo, nisi, eligendi, vero non asperiores numquam recusandae fugit esse! Vero, officia dolores minima placeat quod atque ullam.
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas assumenda cum iste nam, blanditiis laboriosam, omnis esse illo eaque molestias neque rem iure maxime maiores praesentium nemo accusamus, tenetur fuga?
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit culpa, aliquid nam ab distinctio provident earum voluptas delectus tempora voluptate? Velit repudiandae voluptate vel. Nisi voluptatibus ipsum a omnis ullam.
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci quo impedit est illo, nisi, eligendi, vero non asperiores numquam recusandae fugit esse! Vero, officia dolores minima placeat quod atque ullam.
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas assumenda cum iste nam, blanditiis laboriosam, omnis esse illo eaque molestias neque rem iure maxime maiores praesentium nemo accusamus, tenetur fuga?
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit culpa, aliquid nam ab distinctio provident earum voluptas delectus tempora voluptate? Velit repudiandae voluptate vel. Nisi voluptatibus ipsum a omnis ullam.
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci quo impedit est illo, nisi, eligendi, vero non asperiores numquam recusandae fugit esse! Vero, officia dolores minima placeat quod atque ullam.
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas assumenda cum iste nam, blanditiis laboriosam, omnis esse illo eaque molestias neque rem iure maxime maiores praesentium nemo accusamus, tenetur fuga?
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit culpa, aliquid nam ab distinctio provident earum voluptas delectus tempora voluptate? Velit repudiandae voluptate vel. Nisi voluptatibus ipsum a omnis ullam.
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci quo impedit est illo, nisi, eligendi, vero non asperiores numquam recusandae fugit esse! Vero, officia dolores minima placeat quod atque ullam.
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas assumenda cum iste nam, blanditiis laboriosam, omnis esse illo eaque molestias neque rem iure maxime maiores praesentium nemo accusamus, tenetur fuga?
-      
-    </section>
-    // <main>
-    //   <Suspense fallback={<LoadingHeaderSkeleton />}>
-    //     <Header />
-    //   </Suspense>
-      
-    //   <div className="p-4 container mx-auto pt-16">
-    //     <div className="flex items-center justify-between">
-    //       <h1 className="text-3xl font-bold">Posts</h1>
-    //       <Link href={'/t'} >test</Link>
-    //       { user && (
-    //         <div className="py-2">
-    //           <Link href={'/post/create'} className={cn(
-    //             'bg-primary',
-    //             buttonVariants({
-    //               variant: 'default',
-    //             })
-    //           )}>
-    //             Create post
-    //           </Link>
-    //         </div>
-    //       )}
-    //     </div>
-    //     <Suspense fallback={<PostsSkeleton />}>
-    //       <Posts />
-    //     </Suspense>
-    //   </div>
-    // </main>
+    <>
+      <h1 className='font-bold text-3xl md:text-4xl'>Your feed</h1>
+      <div className='grid grid-cols-1 md:grid-cols-3 gap-y-4 md:gap-x-4 py-6'>
+        {session ? <CustomFeed /> : <GeneralFeed />}
+
+        {/* subreddit info */}
+        <div className='overflow-hidden h-fit rounded-lg border border-border order-first md:order-last shadow-lg'>
+          <div className='bg-primary/70 px-2 py-1'>
+            <p className='font-semibold py-3 flex items-center gap-2 '>
+              <HomeIcon className='h-4 w-4' />
+              Home
+            </p>
+          </div>
+          <div className='px-6 py-4 text-sm leading-6'>
+            <div className='flex justify-between gap-x-4 py-3'>
+              <p className='text-foreground'>
+                Your personal Breadit frontpage. Come here to check in with your
+                favorite communities.
+              </p>
+            </div>
+
+            <Link
+              className={buttonVariants({
+                className: 'w-full mt-4 mb-6',
+              })}
+              href={`/r/create`}>
+              Create Community
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
