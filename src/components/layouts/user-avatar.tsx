db6056a360628e2d2fc3f62fa3f626719188cbbc
hayama <hayamaroom@gmail.com>
2024-04-11 10:04:18 +0800

    add tooltip

--- src/components/layouts/user-avatar.tsx
+++ src/components/layouts/user-avatar.tsx
@@ -3,6 +3,7 @@ import { AvatarProps } from '@radix-ui/react-avatar'
 import { Avatar, AvatarFallback } from '@/components/ui/avatar'
 import Image from 'next/image'
 import { Icons } from './icons'
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '../ui/tooltip'
 
 interface UserAvatarProps extends AvatarProps {
   user: Pick<User, 'image' | 'name'>
@@ -10,23 +11,32 @@ interface UserAvatarProps extends AvatarProps {
 
 export default function UserAvatar({ user, ...props }: UserAvatarProps) {
   return (
-    <Avatar {...props}>
-      {user.image ? (
-        <div className='relative aspect-square h-full w-full'>
-          <Image
-            fill
-            sizes='100%'
-            src={user.image}
-            alt='profile picture'
-            referrerPolicy='no-referrer'
-          />
-        </div>
-      ) : (
-        <AvatarFallback>
-          <span className='sr-only'>{user?.name}</span>
-          <Icons.user className='h-4 w-4' />
-        </AvatarFallback>
-      )}
-    </Avatar>
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Avatar {...props}>
+            {user.image ? (
+              <div className='relative aspect-square h-full w-full'>
+                <Image
+                  fill
+                  sizes='100%'
+                  src={user.image}
+                  alt='profile picture'
+                  referrerPolicy='no-referrer'
+                />
+              </div>
+            ) : (
+              <AvatarFallback>
+                <span className='sr-only'>{user?.name}</span>
+                <Icons.user className='h-4 w-4' />
+              </AvatarFallback>
+            )}
+          </Avatar>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>{user.name}</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   )
 }
