413650c33ea0a624c3a4e5de1ee9518c0636c0ee
hayama <hayamaroom@gmail.com>
2024-04-11 09:58:23 +0800

    change user avatar

--- src/components/layouts/user-account-nav.tsx
+++ src/components/layouts/user-account-nav.tsx
@@ -1,19 +1,23 @@
 import { User } from 'next-auth'
 import React from 'react'
 import LogoutButton from './logout-btn';
+import { DropdownMenu, DropdownMenuTrigger } from '../ui/dropdown-menu';
+import UserAvatar from './user-avatar';
 
 interface UserAccountNavProps extends React.HTMLAttributes<HTMLDivElement> {
   user: Pick<User, 'name' | 'image' | 'email'>
 }
 
-export default function UserAccountNav({ 
-  user 
-}: UserAccountNavProps) {
-  console.log(user);
+export default function UserAccountNav({ user }: UserAccountNavProps) {
   
   return (
-    <div>
-      <LogoutButton />
-    </div>
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <UserAvatar 
+          user={{ name: user.name || null, image: user.image || null }}
+          className='h-8 w-8'
+        />
+      </DropdownMenuTrigger>
+    </DropdownMenu>
   )
 }
